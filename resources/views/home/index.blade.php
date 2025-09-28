b6ad3e9b66db28156f87699208a714807ccaa1c5
jvssteam <maulanaridho199@gmail.com>
2025-09-28 15:21:32 +0700

    aja

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -645,8 +645,9 @@
                     <div class="col-sm-6 col-md-4 col-xl-4 p-b-30 m-lr-auto">
                         <!-- Block1 -->
                         <div class="block1 wrap-pic-w">
-                            <img src="{{ category_image_url($kategori->foto_url) }}" alt="{{ $kategori->nama }}"
-                                loading="lazy">
+                            <img src="{{ $category->foto_url }}" alt="{{ $kategori->nama }}" loading="lazy">
+                            {{-- <img src="{{ category_image_url($kategori->foto_url) }}" alt="{{ $kategori->nama }}"
+                                loading="lazy"> --}}
 
                             <div class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
                                 <div class="block1-txt-child1 flex-col-l">
